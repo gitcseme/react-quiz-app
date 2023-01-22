a1f71e455ec679f790479e5ff98375aa4d20d813
Md. Kawsarul Alam Shuvo <shuvo_CSE23@yahoo.com>
2023-01-22 15:19:23 +0600

    First commit to learning react quiz app

---
+++ src/components/Layout.js
@@ -0,0 +1,15 @@
+﻿import Nav from "./Nav";
+import classes from "../styles/Layout.module.css";
+
+export default function Layout({children}) {
+    return (
+        <>
+            <Nav />
+            <main className={classes.main}>
+                <div className="container">
+                    {children}
+                </div>
+            </main>
+        </>
+    );
+}
