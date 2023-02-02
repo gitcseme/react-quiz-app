6ad8f86d1c0b53e748a6b281c93d079149c17f0f
gitcseme <shuvo_CSE23@yahoo.com>
2023-02-02 09:32:21 +0600

    Added firebase configuration

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const app = initializeApp({
+    apiKey: process.env.REACT_API_KEY,
+    authDomain: process.env.REACT_API_AUTH_DOMAIN,
+    projectId: process.env.REACT_API_PROJECT_ID,
+    storageBucket: process.env.REACT_API_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_API_MASSAGING_SENDER_ID,
+    appId: process.env.REACT_API_APP_ID,
+});
+
+export default app;
